7a877d964045fbe3c7d54cec749294653f5f8342
Robert Rice <rrice1291@gmail.com>
2019-03-05 21:31:50 -0600

    just starting

---
+++ FavoriteThings/Games.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FavoriteThings
+{
+    class Games
+    {
+
+        //properties
+        public string Title { get; set; }
+        public Genre Color { get; set; }
+
+
+    }
+
+    enum Genre
+    {
+        RPG,
+        FPS,
+        RTS,
+        MMO
+    }
+}
